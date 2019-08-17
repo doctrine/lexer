95f606c0ed27568ab3145abd9f61227e505316d8
AbdelkaderBah <jr.abdelkader@gmail.com>
2019-08-17 04:39:45 +0000

    Add braces to "if" statement

--- lib/Doctrine/Common/Lexer/AbstractLexer.php
+++ lib/Doctrine/Common/Lexer/AbstractLexer.php
@@ -316,8 +316,9 @@ abstract class AbstractLexer
     public function isCatchedByKey($value, $key)
     {
         if(!isset($this->getCatchablePatterns()[$key]) || !$value || !$this->input)
+        {
             return false;
-
+        }
 
         $flags   = PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_OFFSET_CAPTURE;
         $regex = sprintf('/(%s)/%s', $this->getCatchablePatterns()[$key], $this->getModifiers());
