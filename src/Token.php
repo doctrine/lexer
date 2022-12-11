8a828880f9d7c0478117b3b5ad8889cf2e77f98a
Grégoire Paris <postmaster@greg0ire.fr>
2022-12-11 12:58:57 +0100

    Remove deprecation layer

--- src/Token.php
+++ src/Token.php
@@ -4,18 +4,12 @@ declare(strict_types=1);
 
 namespace Doctrine\Common\Lexer;
 
-use ArrayAccess;
-use Doctrine\Deprecations\Deprecation;
-use ReturnTypeWillChange;
 use UnitEnum;
 
 use function in_array;
 
-/**
- * @template T of UnitEnum|string|int
- * @implements ArrayAccess<string,mixed>
- */
-final class Token implements ArrayAccess
+/** @template T of UnitEnum|string|int */
+final class Token
 {
     /**
      * The string value of the token in the input string
@@ -56,74 +50,5 @@ final class Token implements ArrayAccess
     public function isA(...$types): bool
     {
         return in_array($this->type, $types, true);
-    }
-
-    /**
-     * @deprecated Use the value, type or position property instead
-     * {@inheritDoc}
-     */
-    public function offsetExists($offset): bool
-    {
-        Deprecation::trigger(
-            'doctrine/lexer',
-            'https://github.com/doctrine/lexer/pull/79',
-            'Accessing %s properties via ArrayAccess is deprecated, use the value, type or position property instead',
-            self::class
-        );
-
-        return in_array($offset, ['value', 'type', 'position'], true);
-    }
-
-    /**
-     * @deprecated Use the value, type or position property instead
-     * {@inheritDoc}
-     *
-     * @param array-key $offset
-     *
-     * @return mixed
-     */
-    #[ReturnTypeWillChange]
-    public function offsetGet($offset)
-    {
-        Deprecation::trigger(
-            'doctrine/lexer',
-            'https://github.com/doctrine/lexer/pull/79',
-            'Accessing %s properties via ArrayAccess is deprecated, use the value, type or position property instead',
-            self::class
-        );
-
-        return $this->$offset;
-    }
-
-    /**
-     * @deprecated no replacement planned
-     * {@inheritDoc}
-     */
-    public function offsetSet($offset, $value): void
-    {
-        Deprecation::trigger(
-            'doctrine/lexer',
-            'https://github.com/doctrine/lexer/pull/79',
-            'Setting %s properties via ArrayAccess is deprecated',
-            self::class
-        );
-
-        $this->$offset = $value;
-    }
-
-    /**
-     * @deprecated no replacement planned
-     * {@inheritDoc}
-     */
-    public function offsetUnset($offset): void
-    {
-        Deprecation::trigger(
-            'doctrine/lexer',
-            'https://github.com/doctrine/lexer/pull/79',
-            'Setting %s properties via ArrayAccess is deprecated',
-            self::class
-        );
-
-        $this->$offset = null;
     }
 }
--- tests/TokenTest.php
+++ tests/TokenTest.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace Doctrine\Tests\Common\Lexer;
 
 use Doctrine\Common\Lexer\Token;
-use Doctrine\Deprecations\PHPUnit\VerifyDeprecations;
 use PHPUnit\Framework\TestCase;
 
 final class TokenTest extends TestCase
 {
-    use VerifyDeprecations;
-
     public function testIsA(): void
     {
         /** @var Token<'string'|'int'> $token */
@@ -20,35 +17,5 @@ final class TokenTest extends TestCase
         self::assertTrue($token->isA('string'));
         self::assertTrue($token->isA('int', 'string'));
         self::assertFalse($token->isA('int'));
-    }
-
-    public function testOffsetGetIsDeprecated(): void
-    {
-        $token = new Token('foo', 'string', 1);
-        self::expectDeprecationWithIdentifier('https://github.com/doctrine/lexer/pull/79');
-        self::assertSame('foo', $token['value']);
-    }
-
-    public function testOffsetExistsIsDeprecated(): void
-    {
-        $token = new Token('foo', 'string', 1);
-        self::expectDeprecationWithIdentifier('https://github.com/doctrine/lexer/pull/79');
-        self::assertTrue(isset($token['value']));
-    }
-
-    public function testOffsetSetIsDeprecated(): void
-    {
-        $token = new Token('foo', 'string', 1);
-        self::expectDeprecationWithIdentifier('https://github.com/doctrine/lexer/pull/79');
-        $token['value'] = 'bar';
-        self::assertSame('bar', $token->value);
-    }
-
-    public function testOffsetUnsetIsDeprecated(): void
-    {
-        $token = new Token('foo', 'string', 1);
-        self::expectDeprecationWithIdentifier('https://github.com/doctrine/lexer/pull/79');
-        unset($token['value']);
-        self::assertNull($token->value);
     }
 }
